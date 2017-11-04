381408765db9aa1b4e09a70e06d059ea3ca39d5d
rafalpasiut <rafalpasiut@gmail.com>
2017-11-04 21:37:05 +0100

    sito erastothenesa

---
+++ src/main/java/com/kodilla/SieveOfErastothenes/ErastothenesRunner.java
@@ -0,0 +1,8 @@
+package com.kodilla.SieveOfErastothenes;
+
+public class ErastothenesRunner {
+    public static void main(String[] args){
+        SieveOfErastothenes sieve = new SieveOfErastothenes();
+        System.out.println(sieve.getFirstNumbers(59));
+    }
+}
---
+++ src/main/java/com/kodilla/SieveOfErastothenes/SieveOfErastothenes.java
@@ -0,0 +1,36 @@
+package com.kodilla.SieveOfErastothenes;
+
+import java.util.ArrayList;
+
+public class SieveOfErastothenes {
+
+    private int[] generateNumbers(int n){
+        int[] numbers = new int[n];
+        for(int i=1;i<n;i++){
+            numbers[i]=i;
+        }
+        return numbers;
+    }
+
+    public ArrayList<Integer> getFirstNumbers(int n){
+        int[] numbers;
+        numbers = generateNumbers(n);
+        numbers[1]=0;
+        int j;
+        for(int i=2;(double)i<Math.sqrt(n);i++){
+            j=i*i;
+            while(j<=n){
+                numbers[j]=0;
+                j+=i;
+            }
+            i++;
+        }
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for(int number : numbers){
+            if(number !=0){
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
+}
