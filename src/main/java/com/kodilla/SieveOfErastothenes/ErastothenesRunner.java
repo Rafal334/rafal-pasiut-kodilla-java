cf39ca2cd30d5b27f7c4f1596b0af0fe45d8ef90
rafalpasiut <rafalpasiut@gmail.com>
2017-11-04 21:44:41 +0100

    sieve, typo

--- src/main/java/com/kodilla/SieveOfErastothenes/ErastothenesRunner.java
+++ src/main/java/com/kodilla/SieveOfErastothenes/ErastothenesRunner.java
@@ -3,6 +3,6 @@ package com.kodilla.SieveOfErastothenes;
 public class ErastothenesRunner {
     public static void main(String[] args){
         SieveOfErastothenes sieve = new SieveOfErastothenes();
-        System.out.println(sieve.getFirstNumbers(59));
+        System.out.println(sieve.getPrimeNumbers(59));
     }
 }
