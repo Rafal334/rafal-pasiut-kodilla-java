682269b17d58c3b571e58a50a371acecffd64538
rafalpasiut <rafalpasiut@gmail.com>
2017-11-07 22:03:49 +0100

    junit test class, corrected gradle

--- kodilla-testing/scr/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/scr/main/java/com/kodilla/testing/TestingMain.java
@@ -7,32 +7,5 @@ public class TestingMain {
 
     public static void main(String[] args){
 
-        System.out.println("SimpleUser tests:");
-        String testUserName = "theForumUser";
-
-        SimpleUser simpleUser = new SimpleUser(testUserName);
-        String result = simpleUser.getUsername();
-
-        if(testUserName.equals(result)){
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
-
-        System.out.println("Calculator tests:");
-        Calculator calculator = new Calculator();
-        int a=10, b=5;
-
-        if(calculator.add(a,b)==(a+b)){
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
-
-        if(calculator.subtract(a,b)==(a-b)){
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
     }
 }
--- kodilla-testing/scr/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/scr/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -1,4 +1,21 @@
 package com.kodilla.testing.forum;
 
+import com.kodilla.testing.user.SimpleUser;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ForumTestSuite {
+
+    @Test
+    public void testCaseUsername(){
+        //Given
+        String testUsername = "theForumUser";
+        SimpleUser simpleUser = new SimpleUser(testUsername);
+
+        //When
+        String result = simpleUser.getUsername();
+
+        //Then
+        Assert.assertEquals(testUsername,result);
+    }
 }
