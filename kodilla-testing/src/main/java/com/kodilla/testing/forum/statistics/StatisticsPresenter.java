978b7cdbcfa222cb8befe9d850e9330f2c2589ea
karol <88lkarol@gmail.com>
2017-11-09 23:28:40 +0100

    komentarze, poprawki, hacki do 6.5

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsPresenter.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsPresenter.java
@@ -1,28 +1,24 @@
 package com.kodilla.testing.forum.statistics;
 
 public class StatisticsPresenter {
+    private Statistics statistics;
+    private Integer userNo = 0;
+    private Integer postsNo = 0;
+    private Integer comentsNo = 0;
+    private Double meanPostsPerUser = 0.0;
+    private Double meanCommentsPerUser = 0.0;
+    private Double meanCommentsPerPost = 0.0;
 
-    private Integer userNo;
-    private Integer postsNo;
-    private Integer comentsNo;
-    private Double meanPostsPerUser;
-    private Double meanCommentsPerUser;
-    private Double meanCommentsPerPost;
-
-    public StatisticsPresenter() {
-        userNo = 0;
-        postsNo = 0;
-        comentsNo = 0;
-        meanPostsPerUser = 0.0;
-        meanCommentsPerUser = 0.0;
-        meanCommentsPerPost = 0.0;
+    //aczkolwiek w realnym życiu niezbedny, @InjectMock używa mechanizmu refleksji(taki hack), wiec w tym przypadku zadziała nawet bez tego konstruktora
+    public StatisticsPresenter(Statistics statistics) {
+        this.statistics = statistics;
     }
 
-    public void calculateAdvStatistics(Statistics statistics){
-        if(statistics!=null) {
-            if(statistics.usersNames()!=null) {
+    public void calculateAdvStatistics() {
+        if (statistics != null) {
+            if (statistics.usersNames() != null) {
                 userNo = statistics.usersNames().size();
-            }else{
+            } else {
                 userNo = 0;
             }
             postsNo = statistics.postsCount();
@@ -39,7 +35,7 @@ public class StatisticsPresenter {
             } else {
                 meanCommentsPerPost = 0.0;
             }
-        }else{
+        } else {
             userNo = 0;
             postsNo = 0;
             comentsNo = 0;
@@ -49,14 +45,14 @@ public class StatisticsPresenter {
         }
     }
 
-    public void ShowStatistics(){
+    public void ShowStatistics() {
         System.out.println("Forum statistics:");
         System.out.println("Users count: " + userNo);
         System.out.println("Posts count:" + postsNo);
         System.out.println("Comments count: " + comentsNo);
-        System.out.println("Average posts per user: " + String.format("%.2f",meanPostsPerUser));
-        System.out.println("Average comments per user: " + String.format("%.2f",meanCommentsPerUser));
-        System.out.println("Average comments per post: " + String.format("%.2f",meanCommentsPerPost));
+        System.out.println("Average posts per user: " + String.format("%.2f", meanPostsPerUser));
+        System.out.println("Average comments per user: " + String.format("%.2f", meanCommentsPerUser));
+        System.out.println("Average comments per post: " + String.format("%.2f", meanCommentsPerPost));
     }
 
     public Double getMeanPostsPerUser() {
--- kodilla-testing/src/test/java/com/kodilla/testing/statistics/StatisticsPresenterTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/statistics/StatisticsPresenterTestSuite.java
@@ -2,244 +2,224 @@ package com.kodilla.testing.statistics;
 
 import com.kodilla.testing.forum.statistics.Statistics;
 import com.kodilla.testing.forum.statistics.StatisticsPresenter;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class StatisticsPresenterTestSuite {
 
-    private List<String> generateRandomUsers(int usersNo){
-        List<String> users = new ArrayList<>();
+    //to nie jest blad tylko chcialem ci pokazac ze czesto pojawiajace sie wartosci w testach powinno sie trzymac w stałych
+    private static final Double EXPECTED_DOUBLE_ZERO = 0.0;
+    private static final Double EXPECTED_DOUBLE_ONE = 1.0;
+    private static final Double EXPECTED_DOUBLE_TWO = 2.0;
 
-        for(int i=0;i<usersNo;i++){
-            users.add("User"+i+1);
-        }
-        return users;
-    }
+    @Mock
+    private Statistics StatisticMock;
+
+    @InjectMocks
+    private StatisticsPresenter statisticsPresenter;
 
     @BeforeClass
-    public static void beforeAllTests(){
+    public static void beforeAllTests() {
         System.out.println("Starting tests for: " + StatisticsPresenter.class);
     }
 
     @AfterClass
-    public static void afterAllTest(){
-        System.out.println("Tests finished, class: "+ StatisticsPresenter.class);
+    public static void afterAllTest() {
+        System.out.println("Tests finished, class: " + StatisticsPresenter.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testCalculateAdvStatisticsRandValues(){
+    public void testCalculateAdvStatisticsRandValues() {
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(5));
-        when(statisticsMock.commentsCount()).thenReturn(10);
-        when(statisticsMock.postsCount()).thenReturn(5);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(5));
+        when(StatisticMock.commentsCount()).thenReturn(10);
+        when(StatisticMock.postsCount()).thenReturn(5);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)2.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)1.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)2.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_TWO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ONE, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_TWO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsAllZeros(){
+    public void testCalculateAdvStatisticsAllZeros() {
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(0));
-        when(statisticsMock.commentsCount()).thenReturn(0);
-        when(statisticsMock.postsCount()).thenReturn(0);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(0));
+        when(StatisticMock.commentsCount()).thenReturn(0);
+        when(StatisticMock.postsCount()).thenReturn(0);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsCommentsZero(){
+    public void testCalculateAdvStatisticsCommentsZero() {
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(5));
-        when(statisticsMock.commentsCount()).thenReturn(0);
-        when(statisticsMock.postsCount()).thenReturn(5);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(5));
+        when(StatisticMock.commentsCount()).thenReturn(0);
+        when(StatisticMock.postsCount()).thenReturn(5);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)1.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ONE, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsPostsZero(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(5));
-        when(statisticsMock.commentsCount()).thenReturn(5);
-        when(statisticsMock.postsCount()).thenReturn(0);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsPostsZero() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(5));
+        when(StatisticMock.commentsCount()).thenReturn(5);
+        when(StatisticMock.postsCount()).thenReturn(0);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)1.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_ONE, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsUsersZero(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(0));
-        when(statisticsMock.commentsCount()).thenReturn(10);
-        when(statisticsMock.postsCount()).thenReturn(5);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsUsersZero() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(0));
+        when(StatisticMock.commentsCount()).thenReturn(10);
+        when(StatisticMock.postsCount()).thenReturn(5);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)2.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_TWO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsPosts1000(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(10));
-        when(statisticsMock.commentsCount()).thenReturn(20);
-        when(statisticsMock.postsCount()).thenReturn(1000);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsPosts1000() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(10));
+        when(StatisticMock.commentsCount()).thenReturn(20);
+        when(StatisticMock.postsCount()).thenReturn(1000);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)2.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)100.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.02,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_TWO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals((Double) 100.0, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals((Double) 0.02, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsUsers100(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(100));
-        when(statisticsMock.commentsCount()).thenReturn(20);
-        when(statisticsMock.postsCount()).thenReturn(10);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsUsers100() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(100));
+        when(StatisticMock.commentsCount()).thenReturn(20);
+        when(StatisticMock.postsCount()).thenReturn(10);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)0.2,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)0.1,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)2.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals((Double) 0.2, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals((Double) 0.1, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_TWO, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsCommentOverPosts(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(100));
-        when(statisticsMock.commentsCount()).thenReturn(2000);
-        when(statisticsMock.postsCount()).thenReturn(100);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsCommentOverPosts() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(100));
+        when(StatisticMock.commentsCount()).thenReturn(2000);
+        when(StatisticMock.postsCount()).thenReturn(100);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)20.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)1.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)20.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals((Double) 20.0, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ONE, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals((Double) 20.0, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsPostsOverComments(){
-        //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(100));
-        when(statisticsMock.commentsCount()).thenReturn(2000);
-        when(statisticsMock.postsCount()).thenReturn(5000);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+    public void testCalculateAdvStatisticsPostsOverComments() {
+        //Given        
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(100));
+        when(StatisticMock.commentsCount()).thenReturn(2000);
+        when(StatisticMock.postsCount()).thenReturn(5000);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)20.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)50.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.4,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals((Double) 20.0, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals((Double) 50.0, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals((Double) 0.4, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsNullUserList(){
+    public void testCalculateAdvStatisticsNullUserList() {
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(null);
-        when(statisticsMock.commentsCount()).thenReturn(2000);
-        when(statisticsMock.postsCount()).thenReturn(5000);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+        when(StatisticMock.usersNames()).thenReturn(null);
+        when(StatisticMock.commentsCount()).thenReturn(2000);
+        when(StatisticMock.postsCount()).thenReturn(5000);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)0.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)0.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)0.4,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ZERO, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals((Double) 0.4, statisticsPresenter.getMeanCommentsPerPost());
     }
 
     @Test
-    public void testCalculateAdvStatisticsBIG(){
+    public void testCalculateAdvStatisticsBIG() {
         //Given
-        Statistics statisticsMock = mock(Statistics.class);
-        when(statisticsMock.usersNames()).thenReturn(generateRandomUsers(1000000));
-        when(statisticsMock.commentsCount()).thenReturn(10000000);
-        when(statisticsMock.postsCount()).thenReturn(10000000);
-
-        StatisticsPresenter presenter = new StatisticsPresenter();
+        when(StatisticMock.usersNames()).thenReturn(generateRandomUsers(1000000));
+        when(StatisticMock.commentsCount()).thenReturn(10000000);
+        when(StatisticMock.postsCount()).thenReturn(10000000);
 
         //When
-        presenter.calculateAdvStatistics(statisticsMock);
+        statisticsPresenter.calculateAdvStatistics();
 
         //Then
-        Assert.assertEquals((Double)10.0,presenter.getMeanCommentsPerUser());
-        Assert.assertEquals((Double)10.0,presenter.getMeanPostsPerUser());
-        Assert.assertEquals((Double)1.0,presenter.getMeanCommentsPerPost());
+        Assert.assertEquals((Double) 10.0, statisticsPresenter.getMeanCommentsPerUser());
+        Assert.assertEquals((Double) 10.0, statisticsPresenter.getMeanPostsPerUser());
+        Assert.assertEquals(EXPECTED_DOUBLE_ONE, statisticsPresenter.getMeanCommentsPerPost());
+    }
+
+    //private na koncu
+    private List<String> generateRandomUsers(int usersNo) {
+        //mozna skrocic
+        return IntStream.range(0, usersNo).mapToObj(i -> "User" + i).collect(Collectors.toList());
     }
 }
