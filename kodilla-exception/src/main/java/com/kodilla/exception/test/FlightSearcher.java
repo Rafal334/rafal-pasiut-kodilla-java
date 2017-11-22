511dedbbb8ed3a0ae8788a1d550913b1fa0a2156
rafalpasiut <rafalpasiut@gmail.com>
2017-11-22 20:32:32 +0100

    Submodule 8.4 - corrected map search

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearcher.java
@@ -5,19 +5,18 @@ import java.util.Map;
 
 public class FlightSearcher {
 
-    public boolean findFilght(Flight flight) throws RouteNotFoundException {
+    public boolean findFilght(Flight flight) throws RouteNotFoundException{
 
         if (flight != null) {
             Map<String, Boolean> destinationAirport = new HashMap<>();
             destinationAirport.put("London", true);
             destinationAirport.put("Warszawa", true);
             //destinationAirport.put("New York", true);
-            for (Map.Entry entry : destinationAirport.entrySet()) {
-                if (entry.getKey().equals(flight.getArrivalAirport())) {
-                    return (boolean) entry.getValue();
-                }
+            if(destinationAirport.containsKey(flight.arrivalAirport)){
+                return true;
+            } else{
+                throw new RouteNotFoundException();
             }
-            throw new RouteNotFoundException();
         }
         return false;
     }
