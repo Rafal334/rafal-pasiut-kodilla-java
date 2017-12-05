af04b8319ac14671107d71f7e42189b4d3c4810e
rafalpasiut <rafalpasiut@gmail.com>
2017-12-05 19:54:57 +0100

    module 10.1

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String userName;
+
+    public ForumUser() {
+        this.userName = "John Smith";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
---
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/Shape.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.shape;
+
+import org.springframework.stereotype.Component;
+
+public interface Shape {
+
+    String getShapeName();
+}
