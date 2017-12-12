1e701f6f957eafcc7b22d942eb6b29ac6f288226
rafalpasiut <rafalpasiut@gmail.com>
2017-12-12 18:34:13 +0100

    singleton 11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+public class LoggerTestSuite {
+
+    @Test
+    public void testLoggerSimpleLog() {
+        //Given
+        String logMessage = "Sample log";
+        //When
+        Logger.getInstance().log(logMessage);
+        //Then
+        Assert.assertEquals(logMessage, Logger.getInstance().getLastLog());
+    }
+
+    @Test
+    public void testLoggerFewLogs() {
+        //Given
+        String logMessage = "Sample log";
+        //When
+        IntStream.range(0, 10).
+                forEach(i -> Logger.getInstance().log(logMessage));
+        //Then
+        Assert.assertEquals(logMessage, Logger.getInstance().getLastLog());
+    }
+}
