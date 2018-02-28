ee4b56d14910da43981ad376ee70f59f65b90d9d
rafalpasiut <rafalpasiut@gmail.com>
2018-02-28 14:59:49 +0100

    sql view and events

---
+++ kodilla-jdbc/src/main/resources/sql-queries/createStats_BestsellerStatsEvent_BestsellerCountView.sql
@@ -0,0 +1,28 @@
+use kodilla_course;
+
+drop table if exists STATS;
+
+create table STATS (
+	STAT_ID int auto_increment primary key,
+    STAT_DATE datetime not null,
+    STAT varchar(20) not null,
+    VALUE int not null
+);
+
+drop view if exists BESTSELLERS_COUNT;
+
+create view BESTSELLERS_COUNT AS
+select count(*) from books where BESTSELLER = true;
+
+drop event if exists UPDATE_BESTSELLER_STATS;
+
+DELIMITER $$
+
+create event UPDATE_BESTSELLER_STATS
+on schedule every 1 minute
+do begin
+	call UpdateBestSellers();
+    insert into STATS (STAT_DATE, STAT, VALUE) values(now(),"BESTSELLERS", (select * from BESTSELLERS_COUNT));
+end$$
+
+DELIMITER ;
