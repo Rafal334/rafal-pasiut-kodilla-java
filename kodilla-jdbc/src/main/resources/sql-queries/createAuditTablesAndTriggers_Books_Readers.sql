71468fc3a091333def2eb13b73685545db13bfbe
rafalpasiut <rafalpasiut@gmail.com>
2018-02-28 12:36:38 +0100

    sql query

--- kodilla-jdbc/src/main/resources/sql-queries/createAuditTablesAndTriggers_Books_Readers.sql
+++ kodilla-jdbc/src/main/resources/sql-queries/createAuditTablesAndTriggers_Books_Readers.sql
@@ -24,21 +24,23 @@ create trigger books_insert after insert on books
 for each row
 begin
 	insert into books_aud(event_date, event_type, book_id, new_title, new_pubyear, new_bestseller)
-				values(curtime(), "INSERT", new.BOOK_ID, new.TITLE, new.PUBYEAR, new.BESTSELLER);
+	  values(curtime(), "INSERT", new.BOOK_ID, new.TITLE, new.PUBYEAR, new.BESTSELLER);
 end$$
 
 create trigger readers_delete after delete on readers
 for each row
 begin
 	insert into readers_aud(event_date, event_type, reader_id)
-				values(curtime(), "DELETE", old.reader_id);
+	  values(curtime(), "DELETE", old.reader_id);
 end$$
 
 create trigger books_update after update on books
 for each row
 begin
-	insert into books_aud(event_date, event_type, book_id, new_title, new_pubyear, new_bestseller, old_title, old_pubyear, old_bestseller)
-				values(curtime(), "UPDATE", old.BOOK_ID, new.TITLE, new.PUBYEAR, new.BESTSELLER, old.TITLE, old.PUBYEAR, old.BESTSELLER);
+	insert into books_aud(event_date, event_type, book_id, new_title, new_pubyear, new_bestseller,
+                        old_title, old_pubyear, old_bestseller)
+	  values(curtime(), "UPDATE", old.BOOK_ID, new.TITLE, new.PUBYEAR, new.BESTSELLER,
+           old.TITLE, old.PUBYEAR, old.BESTSELLER);
 end$$
 
 DELIMITER ;
@@ -71,21 +73,23 @@ create trigger readers_insert after insert on readers
 for each row
 begin
 	insert into readers_aud(event_date, event_type, reader_id, new_firstname, new_lastname, new_peselid, new_vip_level)
-				values(curtime(), "INSERT", new.reader_id, new.firstname, new.lastname, new.peselid, new.vip_level);
+	  values(curtime(), "INSERT", new.reader_id, new.firstname, new.lastname, new.peselid, new.vip_level);
 end$$
 
 create trigger readers_delete after delete on readers
 for each row
 begin
 	insert into readers_aud(event_date, event_type, reader_id)
-				values(curtime(), "DELETE", old.reader_id);
+	  values(curtime(), "DELETE", old.reader_id);
 end$$
 
 create trigger readers_update after update on readers
 for each row
 begin
-	insert into readers_aud(event_date, event_type, reader_id, new_firstname, new_lastname, new_peselid, new_vip_level, old_firstname, old_lastname, old_peselid, old_vip_level)
-				values(curtime(), "UPDATE", old.reader_id, new.firstname, new.lastname, new.peselid, new.vip_level, old.firstname, old.lastname, old.peselid, old.vip_level);
+	insert into readers_aud(event_date, event_type, reader_id, new_firstname, new_lastname, new_peselid, new_vip_level,
+                          old_firstname, old_lastname, old_peselid, old_vip_level)
+	  values(curtime(), "UPDATE", old.reader_id, new.firstname, new.lastname, new.peselid, new.vip_level,
+                      old.firstname, old.lastname, old.peselid, old.vip_level);
 end$$
 
 DELIMITER ;
